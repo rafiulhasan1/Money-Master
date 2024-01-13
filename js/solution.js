dcdd97cbfe849bbc0e8d290a3704191dc89198c4
rafiulhasan1 <rafiulhasan001@gmail.com>
2024-01-13 18:33:40 +0600

    money master added

---
+++ js/solution.js
@@ -0,0 +1,6 @@
+document.getElementById('calculate-btn').addEventListener('click',function(){
+    const income = document.getElementById('income');
+    const stringIncome = income.value;
+    // console.log(stringIncome)
+    
+})
