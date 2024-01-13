7b1d23c7fad8bc68617c1cba739cf67666eb02de
rafiulhasan1 <rafiulhasan001@gmail.com>
2024-01-13 18:48:21 +0600

    first part added

--- js/solution.js
+++ js/solution.js
@@ -1,6 +1,25 @@
 document.getElementById('calculate-btn').addEventListener('click',function(){
     const income = document.getElementById('income');
     const stringIncome = income.value;
-    // console.log(stringIncome)
-    
+    const numIncome = parseFloat(stringIncome);
+    // console.log(numIncome)
+
+    const food = document.getElementById('food');
+    const stringFood = food.value;
+    const numFood = parseFloat(stringFood);
+
+    const rent = document.getElementById('rent');
+    const stringRent = rent.value;
+    const numRent = parseFloat(stringRent);
+
+    const other = document.getElementById('clothes');
+    const stringOther = other.value;
+    const numOther = parseFloat(stringOther);
+
+    const totalExpence = document.getElementById('total-expense');
+    totalExpence.innerText = (numFood+numRent+numOther);
+
+    const balence = document.getElementById('balance');
+    balence.innerText = (numIncome-(totalExpence.innerText));
+
 })
